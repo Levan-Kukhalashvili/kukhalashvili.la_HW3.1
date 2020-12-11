8567c09860ab68fff5579f32a5bc7d64955f591b
KukhalashviliLA <48877065+KukhalashviliLA@users.noreply.github.com>
2020-12-11 08:52:30 +0300

    Initial Commit

---
+++ kukhalashvili.la_HW3.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kukhalashvili.la_HW3.1
+//
+//  Created by Admin on 11.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kukhalashvili.la_HW3.1/kukhalashvili_la_HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  kukhalashvili_la_HW3_1App.swift
+//  kukhalashvili.la_HW3.1
+//
+//  Created by Admin on 11.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct kukhalashvili_la_HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
